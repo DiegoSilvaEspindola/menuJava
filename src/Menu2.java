1462dadb438da4889f4ac059a2b2c1840f3247ab
Diego <diegosilvaespindola@gmail.com>
2021-03-22 16:18:18 -0300

    commitado no

---
+++ src/Menu2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Menu2 {
+
+    public static void main(String[] args) {
+        int idade;
+        String posicao;
+        double peso;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("digite a idade do jogador e o peso");
+        idade= teclado.nextInt();
+        System.out.println("digite a posição");
+
+    }
+}
